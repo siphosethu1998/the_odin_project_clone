58fa8d44210ef909a815f8211163bdc781e071d4
siphosethu1998 <siphosethu1998@gmail.com>
2024-12-11 16:58:52 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="flex justify-center">
+      <header>
+        <nav className="flex px-8 py-2 space-x-96">
+          <div className="bg-current">main</div>
+          <ul className="flex items-center">
+            <li className="ml-6">All Paths</li>
+            <li className="ml-6">About</li>
+            <li className="ml-6">Community</li>
+            <li className="ml-6">Support us</li>
+            <li className="ml-6">Sign in</li>
+            <li className="ml-6">lightmode/darkmode icon</li>
+            <li className="ml-8 bg-teal-700 px-4 py-2 rounded text-white">Get Started</li>
+          </ul>
+        </nav>
+      </header>
+      <main>
+      </main>
+      <footer>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
